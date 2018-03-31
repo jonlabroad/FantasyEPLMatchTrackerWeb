d35aac67602315e1657ac541c4371999dc4e7e8a
Jonathan LaBroad <jon.labroad@gmail.com>
2018-03-31 08:55:12 -0400

    Shorten long youtube descriptions

--- src/components/VideoHighlight.tsx
+++ src/components/VideoHighlight.tsx
@@ -18,6 +18,11 @@ export default class VideoHighlight extends React.Component<VideoHighlightProps,
             return null;
         }
 
+        var description = snippet.description.substring(0, Math.min(150, snippet.description.length));
+        if (description.length >= 150) {
+            description += " ...";
+        }
+
         var linkUrl = "https://www.youtube.com/watch?v=" + snippet.resourceId.videoId;
         return (
             <div className="d-flex d-flex-inline video-highlight-element">
@@ -31,7 +36,7 @@ export default class VideoHighlight extends React.Component<VideoHighlightProps,
                         </div>
                     </a>
                     <div className="video-highlight-description">
-                        {snippet.description}
+                        {description}
                     </div>
                 </div>
             </div>
