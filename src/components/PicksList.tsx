ac840119eee08746dbbd35dd3a9aa68400515255
Jonathan LaBroad <jon.labroad@gmail.com>
2019-08-09 16:47:10 -0400

    Captainship indicators

--- src/components/PicksList.tsx
+++ src/components/PicksList.tsx
@@ -32,6 +32,16 @@ export default class PicksList extends React.Component<PicksListProps> {
         }
     }
     
+    getCaptain(pick: Pick): string {
+        if (pick.is_captain) {
+            return "C";
+        }
+        else if (pick.is_vice_captain) {
+            return "VC";
+        }
+        return "";
+    }
+
     renderLive(pick: Pick) {
         const liveElement = LiveHelper.getElement(pick.element, this.props.live);
         if (liveElement) {
@@ -46,6 +56,7 @@ export default class PicksList extends React.Component<PicksListProps> {
             <TableRow key={pick.element} className={starter ? "pickslist-starter" : "pickslist-sub"}>
                 <TableCell padding="none">{element ? ScoreCalculator.calculateElementScore(pick, this.props.live): 0}</TableCell>
                 <TableCell padding="none" className="club-icon-cell"><ClubIcon teamCode={this.getTeamCode(pick)}/></TableCell>
+                <TableCell padding="none" className="club-captain-cell">{this.getCaptain(pick)}</TableCell>
                 <TableCell padding="none">{element ? element.web_name : pick.element}</TableCell>
                 <TableCell padding="none">{element ? BootstrapHelper.getPosition(element.element_type) : pick.position}</TableCell>
                 <TableCell padding="none">{this.renderLive(pick)}</TableCell>
@@ -74,15 +85,16 @@ export default class PicksList extends React.Component<PicksListProps> {
                     <TableRow>
                         <TableCell padding="none">Pts</TableCell>
                         <TableCell padding="none">Club</TableCell>
+                        <TableCell padding="none">Cpt</TableCell>
                         <TableCell padding="none">Name</TableCell>
                         <TableCell padding="none">Pos</TableCell>
                         <TableCell padding="none">Activity</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    <TableRow><TableCell padding="none" colSpan={5}><Typography className="starters-header" variant="subtitle1">Starting 11</Typography></TableCell></TableRow>
+                    <TableRow><TableCell padding="none" colSpan={6}><Typography className="starters-header" variant="subtitle1">Starting 11</Typography></TableCell></TableRow>
                     {this.renderPicks(0, 11, true)}
-                    <TableRow><TableCell padding="none" colSpan={5}><Typography className="subs-header" variant="subtitle1">Subs</Typography></TableCell></TableRow>
+                    <TableRow><TableCell padding="none" colSpan={6}><Typography className="subs-header" variant="subtitle1">Subs</Typography></TableCell></TableRow>
                     {this.renderPicks(11, 15, false)}
                 </TableBody>
             </Table>
